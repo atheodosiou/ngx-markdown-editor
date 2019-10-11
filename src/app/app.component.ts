9a2d22a94dc0ac120a76420ce00a56730911e9cc
Anastasios Theodosiou <anastasios.theodosiou@gmail.com>
2019-10-11 17:35:07 +0300

    Add simpleMDE editor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as simpleMDE from 'simplemde';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-simple-mde';
+  simplemdeConfig:any={
+    autofocus: true,
+    
+    insertTexts: {
+      table: ["", "\n\n| Column 1 | Column 2 | Column 3 |\n| -------- | -------- | -------- |\n| Text     | Text      | Text     |\n\n"],
+    },
+    
+    placeholder: "Type here...",
+   
+    toolbar: true,
+    toolbarTips: false,
+  }
 }
--- src/app/ngx-markdown-editor/ngx-markdown-editor.component.ts
+++ src/app/ngx-markdown-editor/ngx-markdown-editor.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, AfterViewChecked, AfterContentChecked, AfterContentInit, Input } from '@angular/core';
+import * as SimpleMDE from 'simplemde';
 @Component({
   selector: 'ngx-markdown-editor',
   templateUrl: './ngx-markdown-editor.component.html',
   styleUrls: ['./ngx-markdown-editor.component.scss']
 })
-export class NgxMarkdownEditorComponent implements OnInit {
+export class NgxMarkdownEditorComponent implements  AfterViewInit{
 
-  constructor() { }
+  simplemde:any;
+  @ViewChild('editor',{static:false}) editor:ElementRef;
+  @Input() config:any;
 
-  ngOnInit() {
+  constructor() { 
+    console.log('constructor')
+  }
+  ngAfterViewInit(): void {
+    this.simplemde = new SimpleMDE({
+      element:this.editor.nativeElement,
+      toolbar: [{
+        name: "bold",
+        action: SimpleMDE.toggleBold,
+        className: "fa fa-bold",
+        title: "Bold",
+      },
+      {
+        name: "italic",
+        action: SimpleMDE.toggleItalic,
+        className: "fa fa-italic",
+        title: "Italic",
+      }
+      ,
+      {
+        name: "table",
+        action: SimpleMDE.drawTable,
+        className: "fa fa-table",
+        title: "Insert Table",
+      },
+      {
+        name: "preview",
+        action: SimpleMDE.togglePreview,
+        className: "fa fa-eye no-disable",
+        title: "Toggle Preview",
+      }
+    ]
+    });
   }
 
+  getValue(value:any){
+    console.log(this.simplemde.value())
+  }
 }
